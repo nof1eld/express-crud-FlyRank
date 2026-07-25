0f56de2917b3aef0057692498cfb67cf31053389
nofield <imadeddine799@gmail.com>
2026-07-25 17:00:57 +0100

    Stage 3: create with validation

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ tasks = [
   { id: 3, title: "Breathe", done: true },
 ];
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 app.get("/", (req, res) => {
   res.status(200).send({ name: "Task API", version: "1.0", endpoints: ["/tasks"] });
 });
@@ -24,9 +27,27 @@ app.get("/tasks/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const task = tasks.find((t) => t.id === id);
   if (!task) {
-    return res.status(404).send("Not found!");
+    return res.status(404).send({ error: `Task ${id} not found`});
   }
   res.status(200).send(task);
+});
+
+app.post("/tasks", (req, res) => {
+  const title = req.body.title;
+  if (!title || title == "") {
+    return res.status(400).send({ error: `Title is missing`})
+  }
+
+  const MaxId = Math.max(...tasks.map(task => task.id));
+  const id = MaxId + 1;
+
+  const task = {
+    id,
+    title,
+    done: false
+  };
+  tasks.push(task)
+  res.status(201).send(task);
 });
 
 app.listen(port, () => {
