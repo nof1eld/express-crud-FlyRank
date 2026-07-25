593c3be1e88aa24bdf80a36647b793ec400fb1b6
nofield <imadeddine799@gmail.com>
2026-07-25 11:35:59 +0100

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+tasks = [
+  { id: 1, title: "Create a todo-list app", done: true },
+  { id: 2, title: "Do laundry", done: false },
+  { id: 3, title: "Breathe", done: true },
+];
+
 app.get("/", (req, res) => {
-  res.send({ name: "Task API", version: "1.0", endpoints: ["/tasks"] });
+  res.status(200).send({ name: "Task API", version: "1.0", endpoints: ["/tasks"] });
 });
 
 app.get("/health", (req, res) => {
-  res.send({ status: "ok" });
+  res.status(200).send({ status: "ok" });
+});
+
+app.get("/tasks", (req, res) => {
+  res.status(200).send(tasks);
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const task = tasks.find((t) => t.id === id);
+  if (!task) {
+    return res.status(404).send("Not found!");
+  }
+  res.status(200).send(task);
 });
 
 app.listen(port, () => {
